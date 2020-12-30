094da7ab29bc93b5f513fc4293fec82a62189cf1
franzqat <franz.f1032@gmail.com>
2020-12-30 15:21:22 +0100

    Update disk-extend.sh
    removed a print

--- disk-extend.sh
+++ disk-extend.sh
@@ -16,9 +16,6 @@ EOF
 sudo mount -a
 sudo xfs_growfs /dev/sda1
 
-ROOT_FS_SIZE=`sudo parted /dev/sda print | sed -n 2p | awk '{print $3;}'`
-echo "The root file system (/) has a size of $ROOT_FS_SIZE"
-echo ""
 ROOT_FS_SIZE=`sudo df -h / | sed -n 2p | awk '{print $2;}'`
 echo "The root file system (/) has a size of $ROOT_FS_SIZE"
 
